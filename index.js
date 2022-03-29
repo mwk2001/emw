758a90c90ffc8a80ce6762b78edd14794e09310e
mwk2001 <mwk2001@gmail.com>
2022-03-28 23:47:42 -0400

    fixed mediaquery

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ fetch("lists")
   .catch(() => (document.getElementById("list").innerHTML = "Error"));
 
 function getText(name) {
-  fetch(name)
-    .then((response) => response.text())
-    .then((data) => (document.getElementById("arti").innerHTML = data))
+  fetch(name) //name = url : 자료가 있는 위치 또는 주소
+    .then((response) => response.text()) //response = 자료내용이 있는 객체를 변수로 지정. text() method를 이용하여 텍스트로 만든 후 이 값을 리턴해줌.
+    .then((data) => (document.getElementById("arti").innerHTML = data)) // data = 위에서 리턴한 값을 변수로 지정. 문서에 랜더함.
     .catch(() => (document.getElementById("arti").innerHTML = "Error"));
 }
 
